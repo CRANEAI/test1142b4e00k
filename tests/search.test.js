227fc684074a20a3a3821980d5997f8f7ac50d7c
Elly <elly@mg.crane.ai>
2018-10-31 22:49:08 -0400

    added tests/search updated project details in new api

---
+++ tests/search.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - search UNIT TESTS
+ * Application Client
+ * Test1
+*/
+
+/* search view has a Label located at the bottom center of the view with text space */
+it("search view has a Label located at the bottom center of the view with text space", () => {
+  /* ToDo developer needs to complete unit test 
+               search view has a Label located at the bottom center of the view with text space
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
