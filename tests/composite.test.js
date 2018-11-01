569bb45f77d102e0116a985713dcdd7eef5d69a4
Elly <elly@mg.crane.ai>
2018-10-31 22:49:12 -0400

    added tests/composite updated project details in new api

---
+++ tests/composite.test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - composite UNIT TESTS
+ * Application Client
+ * Test1
+*/
+
+/* the composite view has a status bar located at the very-top across the full width of the view */
+it("the composite view has a status bar located at the very-top across the full width of the view", () => {
+  /* ToDo developer needs to complete unit test 
+               the composite view has a status bar located at the very-top across the full width of the view
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})
